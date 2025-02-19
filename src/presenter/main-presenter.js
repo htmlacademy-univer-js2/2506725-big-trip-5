0175e0d83e8eec8756eaef08ad41391436c5e3e5
fluffy <klim44@inbox.ru>
2025-02-19 19:53:32 +0500

    Задание "Шаблонизируй это"

--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -1,27 +1,29 @@
-import Point from '../view/point-view.js';
+import PointView from '../view/point-view.js';
 import PointsList from '../view/points-list-view.js';
 import CreateForm from '../view/create-form-view.js';
 import EditForm from '../view/edit-form-view.js';
 import { render } from '../render.js';
 
-const PONTS_AMOUNT = 3;
-
 export default class Presenter {
   createFormViewComponent = new CreateForm();
-  editFormViewComponent = new EditForm();
   pointsListViewComponent = new PointsList();
 
-  constructor({container}) {
+  constructor({container, pointsModel, destinationsModel, offersModel}) {
     this.container = container;
+    this.pointsModel = pointsModel;
+    this.destinationsModel = destinationsModel;
+    this.offersModel = offersModel;
   }
 
   init() {
+    const points = this.pointsModel.getPoints();
+    const destinations = this.destinationsModel.getDestinations();
+    const offers = this.offersModel.getOffers();
     render(this.pointsListViewComponent, this.container);
-    render(this.editFormViewComponent, this.pointsListViewComponent.getElement());
-    render(this.createFormViewComponent, this.pointsListViewComponent.getElement());
-
-    for (let i = 0; i < PONTS_AMOUNT; i++) {
-      render(new Point(), this.pointsListViewComponent.getElement());
+    render(new EditForm(points[0], destinations, offers), this.pointsListViewComponent.getElement());
+    // render(this.createFormViewComponent, this.pointsListViewComponent.getElement());
+    for (const point of points) {
+      render(new PointView(point, destinations, offers), this.pointsListViewComponent.getElement());
     }
   }
 
--- src/view/point-view.js
+++ src/view/point-view.js
@@ -1,33 +1,39 @@
 import { createElement } from '../render.js';
+import { formateDate, getDuration } from '../utils.js';
+import { DATE_FORMAT } from '../const.js';
 
-function createPointTemplate() {
+function createPointTemplate(point, destinations, offers) {
+  const {basePrice, dateFrom, dateTo, isFavorite, type} = point;
+  const pointDestination = destinations.find((d) => d.id === point.destination);
+  const pointOffers = offers.find((offer) => offer.type === type).offers.filter((offer) => point.offers.includes(offer.id));
+
   return `<li class="trip-events__item">
               <div class="event">
-                <time class="event__date" datetime="2019-03-18">MAR 18</time>
+                <time class="event__date" datetime="${formateDate(dateFrom, DATE_FORMAT['full-date'])}">${formateDate(dateFrom, DATE_FORMAT['month-day'])}</time>
                 <div class="event__type">
-                  <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+                  <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
                 </div>
-                <h3 class="event__title">Taxi Amsterdam</h3>
+                <h3 class="event__title">${type} ${pointDestination.name}</h3>
                 <div class="event__schedule">
                   <p class="event__time">
-                    <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+                    <time class="event__start-time" datetime="${formateDate(dateFrom, DATE_FORMAT['full-date-and-time'])}">${formateDate(dateFrom, DATE_FORMAT['hours-minutes'])}</time>
                     &mdash;
-                    <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+                    <time class="event__end-time" datetime="${formateDate(dateTo, DATE_FORMAT['full-date-and-time'])}">${formateDate(dateTo, DATE_FORMAT['hours-minutes'])}</time>
                   </p>
-                  <p class="event__duration">30M</p>
+                  <p class="event__duration">${getDuration(dateFrom, dateTo)}</p>
                 </div>
                 <p class="event__price">
-                  &euro;&nbsp;<span class="event__price-value">20</span>
+                  &euro;&nbsp;<span class="event__price-value">${basePrice}</span>
                 </p>
                 <h4 class="visually-hidden">Offers:</h4>
                 <ul class="event__selected-offers">
-                  <li class="event__offer">
-                    <span class="event__offer-title">Order Uber</span>
-                    &plus;&euro;&nbsp;
-                    <span class="event__offer-price">20</span>
-                  </li>
+                  ${pointOffers.map((offer) => `<li class="event__offer">
+                      <span class="event__offer-title">${offer.title}</span>
+                      &plus;&euro;&nbsp;
+                      <span class="event__offer-price">${offer.price}</span>
+                    </li>`).join('')}
                 </ul>
-                <button class="event__favorite-btn event__favorite-btn--active" type="button">
+                <button class="event__favorite-btn ${isFavorite ? 'event__favorite-btn--active' : ''}" type="button">
                   <span class="visually-hidden">Add to favorite</span>
                   <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
                     <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
@@ -40,9 +46,15 @@ function createPointTemplate() {
             </li>`;
 }
 
-export default class Point {
+export default class PointView {
+  constructor(point, destinations, offers) {
+    this.point = point;
+    this.destinations = destinations;
+    this.offers = offers;
+  }
+
   getTemplate() {
-    return createPointTemplate();
+    return createPointTemplate(this.point, this.destinations, this.offers);
   }
 
   getElement() {
