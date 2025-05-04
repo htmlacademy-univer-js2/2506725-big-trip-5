147cacd1d6228ab3c1bceae539054a8f0c35d56f
fluffy <klim44@inbox.ru>
2025-05-04 19:58:26 +0500

    Меняй-удаляй

--- src/main.js
+++ src/main.js
@@ -2,17 +2,51 @@ import Presenter from './presenter/main-presenter.js';
 import PointsModel from './model/points-model.js';
 import DestinationModel from './model/destinatione-model.js';
 import OffersModel from './model/offers-model.js';
+import FilterPresenter from './presenter/filter-presenter.js';
+import FilterModel from './model/filter-model.js';
+import { render } from './framework/render.js';
+import NewPointButtonView from './view/new-point-button.js';
 
 const filtersContainer = document.body.querySelector('.trip-controls__filters');
 const eventsContainer = document.body.querySelector('.trip-events');
+const siteHeaderElement = document.body.querySelector('.trip-main');
 
 const pointsModel = new PointsModel();
 const destinationsModel = new DestinationModel();
 const offersModel = new OffersModel();
-pointsModel.init();
+const filterModel = new FilterModel();
 destinationsModel.init();
 offersModel.init();
 
-const presenter = new Presenter({eventsContainer, filtersContainer, pointsModel, destinationsModel, offersModel});
+const presenter = new Presenter({
+  eventsContainer: eventsContainer,
+  filtersContainer: filtersContainer,
+  pointsModel: pointsModel,
+  destinationsModel: destinationsModel,
+  offersModel: offersModel,
+  filterModel: filterModel,
+  onNewPointDestroy: handleNewPointFormClose});
+
+const filterPresenter = new FilterPresenter({
+  filterContainer: filtersContainer,
+  filterModel: filterModel,
+  pointsModel: pointsModel
+});
+
+const newPointButtonComponent = new NewPointButtonView({
+  onClick: handleNewPointButtonClick
+});
+
+function handleNewPointFormClose() {
+  newPointButtonComponent.element.disabled = false;
+}
+
+function handleNewPointButtonClick() {
+  presenter.createPoint();
+  newPointButtonComponent.element.disabled = true;
+}
+
+render(newPointButtonComponent, siteHeaderElement);
 
 presenter.init();
+filterPresenter.init();
